001b3a818d3b0a46cfe08dadc5c6efb8cf81bbd7
suprim12 <suprimofficial@yahoo.com>
2021-02-25 13:00:32 +0545

    New Push

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>News</title>
+    <title>News App</title>
     <!--=================== Styles =============================-->
     <link rel="stylesheet" href="./assets/css/main.css" />
     <link rel="manifest" href="manifest.json" />
@@ -35,121 +35,6 @@
          <?php }else{ ?>
             <p>No Content Found</p>
         <?php }?>
-        <!-- <div class="images--item">
-          <img
-            src="https://images.unsplash.com/photo-1611095565995-d09bbf618f6d?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"
-            alt=""
-          />
-          <h1>Image Title</h1>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Consectetur, eum?
-          </p>
-        </div>
-        <div class="images--item">
-          <img
-            src="https://images.unsplash.com/photo-1613929906219-a2173ecec511?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
-            alt=""
-          />
-          <h1>Image Title</h1>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Consectetur, eum?
-          </p>
-        </div>
-        <div class="images--item">
-          <img
-            src="https://images.unsplash.com/photo-1611095560396-89216a352761?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-            alt=""
-          />
-          <h1>Image Title</h1>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Consectetur, eum?
-          </p>
-        </div>
-        <div class="images--item">
-          <img
-            src="https://images.unsplash.com/photo-1611095565995-d09bbf618f6d?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"
-            alt=""
-          />
-          <h1>Image Title</h1>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Consectetur, eum?
-          </p>
-        </div>
-        <div class="images--item">
-          <img
-            src="https://images.unsplash.com/photo-1613963991007-de23ae6be55f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-            alt=""
-          />
-          <h1>Image Title</h1>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Consectetur, eum?
-          </p>
-        </div>
-        <div class="images--item">
-          <img
-            src="https://images.unsplash.com/photo-1613967250754-e2145aa389c5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
-            alt=""
-          />
-          <h1>Image Title</h1>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Consectetur, eum? Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Ratione quasi mollitia, at explicabo asperiores id alias,
-            aliquid illum debitis labore quod ea molestiae, soluta vitae! Rem
-            voluptatum dolores, nam adipisci quas voluptas beatae aperiam
-            explicabo blanditiis quis? Officiis a rerum repudiandae, iusto odit
-            ipsam natus error sint iure amet eum.
-          </p>
-        </div>
-        <div class="images--item">
-          <img
-            src="https://images.unsplash.com/photo-1613985964670-1d5cab6d8cc4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80"
-            alt=""
-          />
-          <h1>Image Title</h1>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Consectetur, eum?
-          </p>
-        </div>
-        <div class="images--item">
-          <img
-            src="https://images.unsplash.com/photo-1613782456409-b088e8d62e2a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=738&q=80"
-            alt=""
-          />
-          <h1>Image Title</h1>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Consectetur, eum?
-          </p>
-        </div>
-        <div class="images--item">
-          <img
-            src="https://images.unsplash.com/photo-1613851169252-874699eadee2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
-            alt=""
-          />
-          <h1>Image Title</h1>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Consectetur, eum?
-          </p>
-        </div>
-        <div class="images--item">
-          <img
-            src="https://images.unsplash.com/photo-1611095565995-d09bbf618f6d?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80"
-            alt=""
-          />
-          <h1>Image Title</h1>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Consectetur, eum?
-          </p>
-        </div> -->
       </div>
     </section>
     <!--=================== Scripts =============================-->
